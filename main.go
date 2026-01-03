7a51cf16a03de54b4d3d2c06dfd93d8c40243f0c
Sergei <kvt500@gmail.com>
2026-01-04 00:14:58 +0500

    simple sql

--- main.go
+++ main.go
@@ -1,16 +1,47 @@
 package main
 
 import (
-	"fmt"
-	"study/feature1"
-	"study/feature2"
+	"context"
 	"study/feature_postgres/simple_connection"
+	"study/simple_sql"
+
+	"github.com/k0kubun/pp"
 )
 
 func main() {
-	fmt.Println("Hello, Git!")
-	feature1.Feature1()
-	feature2.Feature2()
-	simple_connection.CheckConnection()
-	fmt.Println("test")
+	ctx := context.Background()
+	conn, err := simple_connection.CreateConnection(ctx)
+	if err != nil {
+		panic(err)
+	}
+
+	// if err := simple_sql.CreateTable(ctx, conn); err != nil {
+	// 	panic(err)
+	// }
+
+	// if err := simple_sql.InsertRow(
+	// 	ctx,
+	// 	conn,
+	// 	"Обед",
+	// 	"Покущать надо",
+	// 	false,
+	// 	time.Now(),
+	// ); err != nil {
+	// 	panic(err)
+	// }
+
+	// if err := simple_sql.UpdateRow(ctx, conn); err != nil {
+	// 	panic(err)
+	// }
+
+	// if err := simple_sql.DeleteRow(ctx, conn); err != nil {
+	// 	panic(err)
+	// }
+
+	tasks, err := simple_sql.SelectRows(ctx, conn)
+	if err != nil {
+		panic(err)
+	}
+
+	pp.Println(tasks)
 }
